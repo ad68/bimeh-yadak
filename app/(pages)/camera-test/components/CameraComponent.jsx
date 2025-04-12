239024ae38045f372d7d449f3ecd062520d21a77
ad68 <47919905+ad68@users.noreply.github.com>
2025-04-12 19:52:25 +0330

    .

--- app/(pages)/camera-test/components/CameraComponent.jsx
+++ app/(pages)/camera-test/components/CameraComponent.jsx
@@ -7,34 +7,40 @@ const CameraComponent = () => {
     const webcamRef = useRef(null);
     const [facingMode, setFacingMode] = useState('environment');
     const [image, setImage] = useState(null);
-    const [mode, setMode] = useState("camera")
+    const [cameraMode, setCameraMode] = useState(true)
 
     const capture = () => {
         const screenshot = webcamRef.current.getScreenshot();
         setImage(screenshot);
     };
 
-    /*//////     const switchCamera = () => {
+    /*//////const switchCamera = () => {
             setFacingMode((prev) => (prev === 'user' ? 'environment' : 'user'));
         }; */
 
     return (
         <div style={{ textAlign: 'center' }}>
+            {
+                cameraMode && <section className='fixed h-[100%] w-[100%] z-[2000] top-0 left-0 bg-black'>
+                    <Webcam
+                        ref={webcamRef}
+                        audio={false}
+                        screenshotFormat="image/jpeg"
+                        videoConstraints={{ facingMode }}
+                        style={{ width: '100%', height: "100%", borderRadius: 8 }}
+                    />
+                    <section className='w-[100%] flex absolute bottom-[40px] justify-center items-center'>
+                        <button onClick={capture} className='w-[60px] h-[60px] bg-white flex justify-center items-center rounded-full'>
+                            <section className='w-[50px] h-[50px] rounded-full border border-black'></section>
+                        </button>
+                    </section>
 
-            <section className='fixed h-[100%] w-[100%] z-[2000] top-0 left-0 bg-black'>
-                <Webcam
-                    ref={webcamRef}
-                    audio={false}
-                    screenshotFormat="image/jpeg"
-                    videoConstraints={{ facingMode }}
-                    style={{ width: '100%', height: "100%", borderRadius: 8 }}
-                />
-            </section>
+                </section>
+            }
             <div style={{ marginTop: 12 }}>
                 {!image ? (
                     <>
                         <button onClick={capture}>📸 گرفتن عکس</button>
-
                     </>
                 ) : (
                     <>
