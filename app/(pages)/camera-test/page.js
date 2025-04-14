e797afb417149a1eeb31206174d1c27482c28f22
ad68 <47919905+ad68@users.noreply.github.com>
2025-04-14 17:25:39 +0330

    .

--- app/(pages)/camera-test/page.js
+++ app/(pages)/camera-test/page.js
@@ -1,7 +1,5 @@
 import dynamic from 'next/dynamic';
-
 const CameraComponent = dynamic(() => import('./components/CameraComponent'), { ssr: false });
-
 export default function Page() {
     return (
         <div>
